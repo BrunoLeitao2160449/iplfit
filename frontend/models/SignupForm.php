cf272bdfd04ba7c3995f6d3456b5f2ef0ef12221
BrunoLeitao2160449 <2160449@my.ipleiria.pt>
2017-11-02 21:08:27 +0000

    signup
    works

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -68,7 +68,7 @@ class SignupForm extends Model
 
             // the following three lines were added:
             $auth = \Yii::$app->authManager;
-            $authorRole = $auth->getRole('author');
+            $authorRole = $auth->getRole('user');
             $auth->assign($authorRole, $user->getId());
 
             return $user;
