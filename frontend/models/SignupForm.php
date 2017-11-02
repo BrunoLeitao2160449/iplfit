b49a9a05b4c41e2aebf4066b17cb40fc9592b1c0
BrunoLeitao2160449 <2160449@my.ipleiria.pt>
2017-11-02 23:00:42 +0000

    comments

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -41,20 +41,6 @@ class SignupForm extends Model
      *
      * @return User|null the saved model or null if saving fails
      */
-    /*public function signup()
-    {
-        if (!$this->validate()) {
-            return null;
-        }
-
-        $user = new User();
-        $user->username = $this->username;
-        $user->email = $this->email;
-        $user->setPassword($this->password);
-        $user->generateAuthKey();
-
-        return $user->save() ? $user : null;
-    }*/
 
     public function signup()
     {
