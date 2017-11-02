9f88a281e5eff4406aa41915acfc604dfe6a617e
BrunoLeitao2160449 <2160449@my.ipleiria.pt>
2017-11-02 21:01:32 +0000

    yii rabac

--- console/controllers/RbacControlle.php
+++ console/controllers/RbacControlle.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: b_ml_
- * Date: 02/11/2017
- * Time: 20:46
- */
+
+namespace console\controllers;
+
+use Yii;
+use yii\console\Controller;
+
+class RbacController extends Controller
+{
+    public function actionInit()
+    {
+        $auth = Yii::$app->authManager;
+
+        $user = $auth->createRole('user');
+        $auth->add($user);
+
+        $admin = $auth->createRole('admin');
+        $auth->add($admin);
+        $auth->addChild($admin, $user);
+
+        $auth->assign($user, 2);
+        $auth->assign($admin, 1);
+    }
+}
--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -41,18 +41,39 @@ class SignupForm extends Model
      *
      * @return User|null the saved model or null if saving fails
      */
-    public function signup()
+    /*public function signup()
     {
         if (!$this->validate()) {
             return null;
         }
-        
+
         $user = new User();
         $user->username = $this->username;
         $user->email = $this->email;
         $user->setPassword($this->password);
         $user->generateAuthKey();
-        
+
         return $user->save() ? $user : null;
+    }*/
+
+    public function signup()
+    {
+        if ($this->validate()) {
+            $user = new User();
+            $user->username = $this->username;
+            $user->email = $this->email;
+            $user->setPassword($this->password);
+            $user->generateAuthKey();
+            $user->save(false);
+
+            // the following three lines were added:
+            $auth = \Yii::$app->authManager;
+            $authorRole = $auth->getRole('author');
+            $auth->assign($authorRole, $user->getId());
+
+            return $user;
+        }
+
+        return null;
     }
 }
